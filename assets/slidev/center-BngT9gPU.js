f527b1a95d01c3186ce4969f496023f2fdb536e9
HuyVuCV1011 <huyvucv10112000@gmail.com>
2026-09-15 19:15:21 +0700

    Update cover instructors and references

---
+++ assets/slidev/center-BngT9gPU.js
@@ -0,0 +1,1 @@
+import{B as e,L as t,g as n,y as r}from"../modules/shiki-BfF9UUjm.js";import{gt as i}from"../index-YVE8a-2f.js";var a={},o={class:`slidev-layout center h-full grid place-content-center`},s={class:`my-auto`};function c(i,a){return t(),r(`div`,o,[n(`div`,s,[e(i.$slots,`default`)])])}var l=i(a,[[`render`,c]]);export{l as t};
